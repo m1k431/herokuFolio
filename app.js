b52eec0c9bef2cfdfbb6395cda09b7d7051cebb5
m1k431 <garcia.mikaelgmail.com>
2020-06-29 21:21:48 +0200

    eeek ?

--- app.js
+++ app.js
@@ -9,6 +9,68 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+let datetime = new Date(),
+  p0rt = 80,
+  filePath = `./logs/ip${nbLog}.log`,
+  nbUser = 0,
+  logger = log4js.getLogger('trace')
+
+var nbLog = datetime.getFullYear() + String(datetime.getMonthFormatted()) + String(datetime.getDate()) + String(datetime.getHoursFormatted()) + String(datetime.getMinutesFormatted()) + String(datetime.getSecondsFormatted()),
+  ip, geo,
+  expressSessionFileStore = FileStore(session),
+  sess = {
+    genid: function (req) {
+      return uid.sync(18)
+    },
+    store: new FileStore,
+    resave: true,
+    saveUninitialized: true,
+    secret: 'qwerty',
+    cookie: {
+      expires: datetime.setUTCFullYear(datetime.getFullYear() + 1),
+      maxage: datetime.setUTCFullYear(datetime.getFullYear() + 1)
+    },
+    sessionID: 0,
+    horodate: '',
+    ip: '',
+    geoloc: {},
+    pathname: '',
+    nbViews: []
+  }
+
+//mongoDB
+const MongoClient = require('mongodb').MongoClient,
+  uri = "mongodb+srv://snow:<password>@cluster0-5cwg1.mongodb.net/<dbname>?retryWrites=true&w=majority",
+  client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+
+client.connect(err => {
+  const collection = client.db("m1k431").collection("brickBreaker")
+  // perform actions on the collection object
+  //console.log(collection)
+  /*collection.insertOne({
+      visitorName: 'mika',
+      score: '123'
+  })*/
+  client.close()
+})
+
+//APP.LOGGER_________________________________________________________________
+log4js.configure({
+  appenders: {
+    trace: { type: 'file', filename: `logs/ip${nbLog}.log` }
+  },
+  categories: { default: { appenders: ['trace'], level: 'trace' } }
+})
+
+//APP.FS_________________________________________________________________
+fs.writeFile(filePath, datetime, (err) => {
+  if (err) throw err
+  console.log(`The file ${nbLog}.log was succesfully created`)
+})
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -23,12 +85,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/index.js
+++ routes/index.js
@@ -2,20 +2,62 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
+  nbUser++
+  datetime = new Date()
+  if (!req.session.views) {
+    req.session.views = {}
+  }
+  var pathname = parseurl(req).pathname
+  req.session.views[pathname] = (req.session.views[pathname] || 0) + 1
+  //fix UTC+2 hours
+  datetime.setUTCHours(datetime.getHours())
+  ip = req.connection.remoteAddress
+  geo = geoip.lookup(ip)
+  req.session.sessionID = req.sessionID
+  req.session.horodate = datetime
+  req.session.ip = ip
+  req.session.geoloc = geo
   res.render('index', { title: 'Express' });
+  //LOGGER
+  logger.trace(req.session)
+  console.log(req.session)
 });
 
-router.get('/cv', function(req, res, next) {
+router.get('/cv', function (req, res, next) {
+  //VIEWS
+  pathname = parseurl(req).pathname
+  req.session.views[pathname] = (req.session.views[pathname] || 0) + 1
+  if (req.query.r == 'highScore') {
+    //AJax
+  }
   res.render('cv', { title: 'Express' });
+  logger.trace(pathname + ': ' + req.session.views[pathname])
+  console.log(pathname + ': ' + req.session.views[pathname])
 });
 
-router.get('/aL1v3', function(req, res, next) {
+router.get('/aL1v3', function (req, res, next) {
+  //VIEWS
+  pathname = parseurl(req).pathname
+  req.session.views[pathname] = (req.session.views[pathname] || 0) + 1
+  if (req.query.r == 'highScore') {
+    //AJax
+  }
   res.render('aL1v3', { title: 'Express' });
+  logger.trace(pathname + ': ' + req.session.views[pathname])
+  console.log(pathname + ': ' + req.session.views[pathname])
 });
 
-router.get('/giftedADHD', function(req, res, next) {
+router.get('/giftedADHD', function (req, res, next) {
+  //VIEWS
+  pathname = parseurl(req).pathname
+  req.session.views[pathname] = (req.session.views[pathname] || 0) + 1
+  if (req.query.r == 'highScore') {
+    //AJax
+  }
   res.render('giftedADHD', { title: 'Express' });
+  logger.trace(pathname + ': ' + req.session.views[pathname])
+  console.log(pathname + ': ' + req.session.views[pathname])
 });
 
 module.exports = router;
