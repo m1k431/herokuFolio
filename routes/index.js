166d7ea0b68a0dfd9dee9ab4b06f4cf7252e3d3f
m1k431 <garcia.mikaelgmail.com>
2020-07-03 15:54:20 +0200

    res.end()

--- routes/index.js
+++ routes/index.js
@@ -4,18 +4,22 @@ var router = express.Router()
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'index' })
+  res.end()
 })
 
 router.get('/cv', function (req, res, next) {
   res.render('cv', { title: 'cv' })
+  res.end()
 })
 
 router.get('/aL1v3', function (req, res, next) {
   res.render('aL1v3', { title: 'al1ve' })
+  res.end()
 })
 
 router.get('/giftedADHD', function (req, res, next) {
   res.render('giftedADHD', { title: 'adhd' })
+  res.end()
 })
 
 module.exports = router
