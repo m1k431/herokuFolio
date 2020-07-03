0e1357f844e0e188b42b1698174dd01750b411ab
m1k431 <garcia.mikaelgmail.com>
2020-07-03 19:19:58 +0200

    rollback no count views

--- routes/index.js
+++ routes/index.js
@@ -1,45 +1,21 @@
-var express = require('express'),
-  parseurl = require('parseurl'),
-  geoip = require('geoip-lite'),
-  router = express.Router(),
-  sess = req.session
-
+var express = require('express')
+var router = express.Router()
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  datetime = new Date()
-  //VIEWS
-  if (!req.session.views) {
-    req.session.views = {}
-  }
-  var pathname = parseurl(req).pathname
-  req.session.views[pathname] = (req.session.views[pathname] || 0) + 1
-  //fix UTC+2 hours
-  datetime.setUTCHours(datetime.getHours())
-  ip = req.connection.remoteAddress
-  geo = geoip.lookup(ip)
-  sess.horodate = datetime
-  sess.ip = ip
-  sess.geoloc = geo
-  sess.views = req.session.views
   res.render('index', { title: 'index' })
-  logger.trace(sess)
-  console.log(sess)
 })
 
 router.get('/cv', function (req, res, next) {
   res.render('cv', { title: 'cv' })
-  res.end()
 })
 
 router.get('/aL1v3', function (req, res, next) {
   res.render('aL1v3', { title: 'al1ve' })
-  res.end()
 })
 
 router.get('/giftedADHD', function (req, res, next) {
   res.render('giftedADHD', { title: 'adhd' })
-  res.end()
 })
 
 module.exports = router
