2045d0042e4a6288dca1d754e9f038fc0398576c
m1k431 <garcia.mikaelgmail.com>
2020-07-03 19:08:22 +0200

    test session /

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,24 @@ var router = express.Router()
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
+  datetime = new Date()
+  //VIEWS
+  if (!req.session.views) {
+    req.session.views = {}
+  }
+  var pathname = parseurl(req).pathname
+  req.session.views[pathname] = (req.session.views[pathname] || 0) + 1
+  //fix UTC+2 hours
+  datetime.setUTCHours(datetime.getHours())
+  ip = req.connection.remoteAddress
+  geo = geoip.lookup(ip)
+  sess.horodate = datetime
+  sess.ip = ip
+  sess.geoloc = geo
+  sess.views = req.session.views
   res.render('index', { title: 'index' })
-  res.end()
+  logger.trace(sess)
+  console.log(sess)
 })
 
 router.get('/cv', function (req, res, next) {
