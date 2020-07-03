7f6fa23af4699bc91975d8c3aa92a95073f2460c
m1k431 <garcia.mikaelgmail.com>
2020-07-03 15:05:18 +0200

    parseurl

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
-var express = require('express')
-var router = express.Router()
+var express = require('express'),
+  parseurl = require('parseurl'),
+  router = express.Router()
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
