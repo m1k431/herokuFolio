41eb8ec075ae23e7ba763b3365cb819718c5c7a4
m1k431 <garcia.mikaelgmail.com>
2020-07-03 15:34:37 +0200

    rollback

--- routes/index.js
+++ routes/index.js
@@ -1,47 +1,9 @@
-var express = require('express'),
-  parseurl = require('parseurl'),
-  geoip = require('geoip-lite'),
-  router = express.Router()
-
-var sess = {
-    genid: function (req) {
-      return uid.sync(18)
-    },
-    store: new FileStore(),
-    resave: true,
-    saveUninitialized: true,
-    secret: 'qwerty',
-    cookie: {
-      expires: datetime.setUTCFullYear(datetime.getFullYear() + 1),
-      maxage: ms('3600 days')
-    },
-    sessionID: 0,
-    horodate: '',
-    ip: '',
-    geoloc: {},
-    pathname: '',
-    nbViews: []
-  }
+var express = require('express')
+var router = express.Router()
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  datetime = new Date()
-  if (!req.session.views) {
-    req.session.views = {}
-  }
-  var pathname = parseurl(req).pathname
-  req.session.views[pathname] = (req.session.views[pathname] || 0) + 1
-  //fix UTC+2 hours
-  datetime.setUTCHours(datetime.getHours())
-  ip = req.connection.remoteAddress
-  geo = geoip.lookup(ip)
-  req.session.horodate = datetime
-  req.session.ip = ip
-  req.session.geoloc = geo
   res.render('index', { title: 'index' })
-  //LOGGER
-  logger.trace(req.session)
-  console.log(req.session)
 })
 
 router.get('/cv', function (req, res, next) {
